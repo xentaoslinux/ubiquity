f59fd4b148fbb813910c7c959a9ca3905db15675
Dindin Hernawan <root@dev.xentaos.org>
2017-12-07 01:53:57 +0700

    Update Uploads

---
+++ depends2build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# .
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# .
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+sudo apt-get install  bf-utf-source check debconf-utils devio dh-di  gobject-introspection intltool  isoquery keymapper libbogl-dev libdebconfclient0-dev  libdebian-installer4-dev  libgirepository1.0-dev libido3-0.1-dev libindicator3-dev libiw-dev  libparted-dev librsvg2-bin libsubunit-dev pep8 pyflakes3  python-gi-dev python-scour python3-all  python3-mock python3-pam ubuntu-artwork xkb-data-i18n xvfb python-rsvg -y
